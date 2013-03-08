02136e5c9225b2fc2c9cf10aad24729bffdb2fae
Lukin <my@lukin.net>
2013-03-08 23:43:52 +0800

    fixbug

--- lib/UCache.php
+++ lib/UCache.php
@@ -25,6 +25,7 @@ class UCache {
 
     public function __construct() {
         if (IS_SAE) {
+            $error_level = error_reporting(0);
             // sae memcache
             if (function_exists('memcache_init')) {
                 $this->object = new MCache();
@@ -38,6 +39,7 @@ class UCache {
             } else {
                 $this->object = new NOOPClass();
             }
+            error_reporting($error_level);
         }
         // file cache
         else {
